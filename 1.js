73be704131699d1f9c700b89fb2ac0316a84ca8e
svishalkumar06 <32671968+svishalkumar06@users.noreply.github.com>
2017-11-13 10:50:08 +0530

    Update 1.js

--- 1.js
+++ 1.js
@@ -8,24 +8,16 @@ My first page
 
  <script type="text/javascript">
  function show(){
-   var no=parseInt(docu<!doctype html>
-<html>
-<head>
-<title>
-My first page
-</title>
-</head>
-
- <script type="text/javascript">
- function show(){
-   var no=parseInt(document.getElementById("a").value);
+   var no=Number(document.getElementById("a").value);
    var result;
-   if(no>0)
+    if(no>0)
      result="positive";
    else if(no==0)
      result="zero";
-   else
+   else if(no<0)
      result="negative"
+     else
+         result="invalid input";
     document.getElementById("demo").innerHTML=result;
  }
   </script>
@@ -34,33 +26,8 @@ My first page
 <h1 id="mytitle">variable and execution of javascript
 </h1>
     <div id="container">
-    <div>First no:
-        <input type="number" id="a"></div> </div>
-    <button onclick="show()">submit</button>
-    <p id="demo"></p>
-    </body>
-</html>
-
-
-
-ment.getElementById("a").value);
-   var result;
-   if(no>0)
-     result="positive";
-   else if(no==0)
-     result="zero";
-   else
-     result=
-    document.getElementById("demo").innerHTML=p;
- }
-  </script>
-
-    <body >
-<h1 id="mytitle">variable and execution of javascript
-</h1>
-    <div id="container">
-    <div>First no:
-<input type="number" id="a"></div>
+    <div>Enter no:
+        <input type="text" id="a"></div> </div>
     <button onclick="show()">submit</button>
     <p id="demo"></p>
     </body>
